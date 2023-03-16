f986d265d41f871451cfac75fb3a38154b1b577d
yassineyb7 <yassinecr666@gmail.com>
2023-03-16 04:52:32 -0700

    done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,10 @@
-#!/bin/bash
+#include <stdio.h>
+/*
+ * main - A program that prints a line with pusts function 
+ * Return: à (Success)
+*/
+int main(void) 
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+} 
