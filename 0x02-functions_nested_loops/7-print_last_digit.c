408ae6a6712fcd18883ef8197fc8031b02fc70d1
yassineyb7 <125898200+yassineyb7@users.noreply.github.com>
2023-03-21 16:00:22 +0000

    done

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -5,14 +5,19 @@
  * @n: the int to extract the last digit from
  * Return: value of the last digit
  */
-int main(void)
-{
-	int r;
 
-	print_last_digit(98);
-	print_last_digit(0);
-	r = print_last_digit(-1024);
-	_putchar('0' + r);
-	_putchar('\n');
-	return (0);
+int print_last_digit(int n)
+{
+	int a;	
+
+	if (n < 0)
+		n = -n;
+
+	a = n % 10;
+		
+	if (a < 0)
+		a = -a;
+
+	_putchar(a + '0');
+	return (a);
 }
